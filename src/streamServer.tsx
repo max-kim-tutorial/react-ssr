1df43f7661764fb7dbba813b55d0cdc0e61af9d7
Max Kim <hwaseen@gmail.com>
2022-09-05 01:22:14 +0900

    Streaming SSR Server

--- src/streamServer.tsx
+++ src/streamServer.tsx
@@ -11,35 +11,60 @@ const app = express();
 
 const bigFilePath = path.resolve(__dirname, '../public/big.file');
 
-// stream: 비동기 요청이 시작될 때 두번째 스트림을 블락한 후, 특정 요청이 완료되면 재개(Streaming SSR)
-app.get('/ssr/stream', async (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-
-  const fileData = await fs.promises.readFile(bigFilePath);
-  res.write(`1-${fileData}\n\n`);
+// Default SSR: 모든 비동기 요청이 끝나야 응답 리턴 (all-or-nothing, waterfall)
+app.get('/ssr/default', async (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
 
   const {
     data: { name },
   } = await axios.get(
-    'https://deelay.me/10000/https://pokeapi.co/api/v2/berry/25/',
+    'https://deelay.me/3000/https://pokeapi.co/api/v2/berry/30/',
   );
-  res.write(`2-${name}\n\n`);
 
-  res.end();
+  res.end(`
+    <div id="content">
+      <h1>Default SSR</h1>
+      <div>non-data fetching UI</div>
+      <div>data fetching UI with response: ${name}</div>
+    </div>  
+  `);
 });
 
-// non-stream: 비동기 요청과 file read까지 모든 것이 완성되어야 응답 리턴(기존 SSR)
-app.get('/ssr/response', async (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  const fileData = await fs.promises.readFile(bigFilePath);
+// Streaming SSR: fallback UI가 담긴 HTML을 스트림 쓰기 이후, 특정 요청이 완료되면 pop-in
+app.get('/ssr/stream', async (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+
+  res.write(`
+    <div id="content">
+      <h1>Streaming SSR</h1>
+      <div>non-data fetching UI</div>
+      <div id="fallback">...loading</div>  
+    </div>
+  `);
 
   const {
     data: { name },
   } = await axios.get(
-    'https://deelay.me/10000/https://pokeapi.co/api/v2/berry/25/',
+    'https://deelay.me/3000/https://pokeapi.co/api/v2/berry/25/',
   );
 
-  res.end(`1-${fileData}\n\n2-${name}\n\n`);
+  res.write(`
+    <div hidden id="pop-in">data fetching UI with response: ${name}</div>
+    <script>
+      const popInHtml = document.getElementById('pop-in');
+      const fallback = document.getElementById('fallback');
+
+      popInHtml.parentNode.removeChild(popInHtml)
+
+      const fallbackParentNode = fallback.parentNode;
+      fallbackParentNode.removeChild(fallback);
+
+      popInHtml.removeAttribute('hidden');
+      fallbackParentNode.appendChild(popInHtml);
+    </script>
+  `);
+
+  res.end();
 });
 
 app.listen(3004, () => console.log('Server started http://localhost:3004'));
